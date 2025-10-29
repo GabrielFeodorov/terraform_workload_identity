a4a4007aac058b05c336e8df54b4de82199afb60
GabrielFeodorov <gabriel.feodorov@oracle.com>
2025-10-29 17:36:29 +0200

    removed vcn

--- tf/main.tf
+++ tf/main.tf
@@ -28,22 +28,22 @@ data "oci_identity_compartment" "test_compartment" {
 
 
 
-module "vcn" {
-  source  = "oracle-terraform-modules/vcn/oci"
-  version = "2.2.0"
+# module "vcn" {
+#   source  = "oracle-terraform-modules/vcn/oci"
+#   version = "2.2.0"
 
-  compartment_id = var.compartment_id
+#   compartment_id = var.compartment_id
 
-  # vcn parameters
-  create_drg               = false
-  internet_gateway_enabled = true
-  lockdown_default_seclist = true
-  nat_gateway_enabled      = false
-  service_gateway_enabled  = false
-  vcn_cidr                 = "10.0.0.0/16"
-  vcn_dns_label            = "wivcn"
-  vcn_name                 = "wivcn"
-}
+#   # vcn parameters
+#   create_drg               = false
+#   internet_gateway_enabled = true
+#   lockdown_default_seclist = true
+#   nat_gateway_enabled      = false
+#   service_gateway_enabled  = false
+#   vcn_cidr                 = "10.0.0.0/16"
+#   vcn_dns_label            = "wivcn"
+#   vcn_name                 = "wivcn"
+# }
 
 provider "oci" {
   auth   = "OKEWorkloadIdentity"
