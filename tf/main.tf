41b5dab7dec13c992779c9123f50eef4db9b6421
GabrielFeodorov <gabriel.feodorov@oracle.com>
2025-10-29 17:13:55 +0200

    backend_oci

--- tf/main.tf
+++ tf/main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "oci" {
+    bucket    = "wi_terraform"
+    namespace = "idjuatm1d4mr"
+    region    = "eu-frankfurt-1"
+    auth      = "OKEWorkloadIdentity"
+  }
 }
 
 
@@ -19,6 +25,25 @@ variable "region" {}
 data "oci_identity_compartment" "test_compartment" {
   #Required
   id = var.compartment_id
+}
+
+
+
+module "vcn" {
+  source  = "oracle-terraform-modules/vcn/oci"
+  version = "2.2.0"
+
+  compartment_id = var.compartment_id
+
+  # vcn parameters
+  create_drg               = false
+  internet_gateway_enabled = true
+  lockdown_default_seclist = true
+  nat_gateway_enabled      = false
+  service_gateway_enabled  = false
+  vcn_cidr                 = "10.0.0.0/16"
+  vcn_dns_label            = "wivcn"
+  vcn_name                 = "wivcn"
 }
 
 provider "oci" {
